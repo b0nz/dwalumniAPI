67867c76ebae0593232cbc540370c104fef57042
emixbal <emixbal@gmail.com>
2018-07-04 14:02:19 +0700

    add something

--- app/settings/local.py
+++ app/settings/local.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dwalumni2',
         'USER': 'root',
-        'PASSWORD': '123qweasd',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
