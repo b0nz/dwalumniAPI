e4e7086e8830eadfbe00b19ca160cdb631f8459f
pleky <onyariantoprapanca@gmail.com>
2018-07-13 16:02:53 +0700

    fix user register

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -22,7 +22,11 @@ class UserSerializer(serializers.ModelSerializer):
             self.validated_data['email'],
             self.validated_data['password']
         )
+        user_group = Group.objects.get(id=1)
+        user.groups.add(user_group)
+        # print(user_group)
         return user
+
 
 class UserSerializerSimple(UserSerializer):
     class Meta:
